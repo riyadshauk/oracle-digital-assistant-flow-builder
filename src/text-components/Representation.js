0cac53876fb5fc66ae41f894f156026d8529b57e
Riyad Shauk <riyad.shauk@gmail.com>
2019-04-11 17:26:36 -0700

    fix to subscribe to store changes

--- src/text-components/Representation.js
+++ src/text-components/Representation.js
@@ -1,12 +1,26 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import { dump } from 'js-yaml';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import store from '../redux/store';
 
-export default () => (
-  <SyntaxHighlighter language="yaml" style={docco}>
-    {dump(store.getState().representation.representation)}
-  </SyntaxHighlighter>
-);
+export default class extends Component<{}, {}> {
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.forceUpdate());
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  unsubscribe: Function;
+
+  render() {
+    return (
+      <SyntaxHighlighter language="yaml" style={docco}>
+        {dump(store.getState().representation.representation)}
+      </SyntaxHighlighter>
+    );
+  }
+}
